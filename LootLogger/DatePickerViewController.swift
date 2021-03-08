19cdd29bfb0a44a3e7c7921b50b92ce729a1e8d5
mcmay <mcmay@126.com>
2021-03-08 13:41:40 +1100

    Added comment in DatePickerViewController.

--- LootLogger/DatePickerViewController.swift
+++ LootLogger/DatePickerViewController.swift
@@ -13,7 +13,8 @@ class DatePickerViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+        // When the view of DatePickerViewController is popped off the navigation controller's
+        // view controller stack, the next view (here the detail view) is pushed on to the stack instead.
         let detailViewController = navigationController?.topViewController as! DetailViewController
         detailViewController.newDate = datePicker.date
     }
