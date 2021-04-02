242effed5d9ddcedec5e7fd55850417a06c07577
mcmay <mcmay2006@gmail.com>
2021-04-02 23:48:27 +1100

    Add files via upload

---
+++ Swift/iOSProg13BigNerd7/Examples/Ch16SizeClass_LootLogger/LootLogger/LootLogger/DetailViewController.swift
@@ -0,0 +1,134 @@
+//
+//  DetailViewController.swift
+//  LootLogger
+//
+//  Created by Chao Mei on 11/2/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate,
+                            UINavigationControllerDelegate,
+                            UIImagePickerControllerDelegate {
+    // It is through the following outlets that the Detail View Controller
+    // in the Main.storyboard grabs possession of this
+    // DetailViewController object
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        return formatter
+    }()
+    var item: Item! { // Property observer
+        didSet {
+            navigationItem.title = item.name
+        }
+    }
+    var imageStore: ImageStore!
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alertController.modalPresentationStyle = .popover
+        // show pointer of altercontroller's view
+        alertController.popoverPresentationController?.barButtonItem = sender
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let camerAction = UIAlertAction(title: "Camera", style: .default) {
+                _ in
+                let imagePicker = self.imagePicker(for: .camera)
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            alertController.addAction(camerAction)
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) {
+            _ in
+            let imagePicker = self.imagePicker(for: .photoLibrary)
+            imagePicker.modalPresentationStyle = .popover
+            imagePicker.popoverPresentationController?.barButtonItem = sender
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        alertController.addAction(photoLibraryAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    func imagePicker (for sourceType: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        return imagePicker
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Get picked image from info dictionary
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        // Store the image in the ImageStore for the item's key
+        imageStore.setImage(image, forKey: item.itemKey)
+        
+        // Put that image on the screen in the image view
+        imageView.image = image
+        
+        // Take image picker off the screen - you must call this dismiss method
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        //valueField.text = "\(item.valueInDollars)"
+        //dateLabel.text = "\(item.dateCreated)"
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+        
+        // Get the item key
+        let key = item.itemKey
+        
+        // If there is an associated image with the item, display it on the image view
+        let imageToDisplay = imageStore.image(forKey: key)
+        imageView.image = imageToDisplay
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Clear first responder
+        view.endEditing(true)
+        
+        // "Save" changes to item
+        item.name = nameField.text ?? ""
+        item.serialNumber = serialNumberField.text
+        if let valueText = valueField.text,
+           let value = numberFormatter.number(from: valueText) {
+            item.valueInDollars = value.intValue
+        } else {
+            item.valueInDollars = 0
+        }
+    }
+}
---
+++ Swift/iOSProg13BigNerd7/Examples/Ch16SizeClass_LootLogger/LootLogger/LootLogger/ItemsViewControler.swift
@@ -0,0 +1,127 @@
+//
+//  ItemsViewControler.swift
+//  LootLogger
+//
+//  Created by Chao Mei on 2/2/21.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController, UIGestureRecognizerDelegate {
+    var itemStore: ItemStore!
+    var imageStore: ImageStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //tableView.rowHeight = 65
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
+        // The current view controller holds the backBarButtonItem of the next view controller. Queer.
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+         
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    @IBAction func addNewItem (_ sender: UIBarButtonItem) {
+        // Make a new index path for the 0th section, last row
+        //let lastRow = tableView.numberOfRows(inSection: 0)
+        //let indexPath = IndexPath(row: lastRow, section: 0)
+        
+        // Insert this new row into the table
+        //tableView.insertRows(at: [indexPath], with: .automatic)
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.firstIndex(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // Insert this new rown into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    /*
+     Table header removed. "Edit" button gone
+     @IBAction func toggleEditingMode (_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }*/
+    override func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    override func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell with default appearance
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        // Now that ItemCell is customized, we shouldn't use UITableViewCell as the identifier.
+        // They are different.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        // Set the text on the cell with description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+        //cell.textLabel?.text = item.name
+        //cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    override func tableView (_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command ...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            // Remove the item from the store
+            itemStore.removeItem(item)
+            
+            // Remove the item's image from the image store
+            imageStore.deleteImage(forKey: item.itemKey)
+            
+            // Also remove that row from the table view with an animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    override func tableView (_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // If the triggered segue is the "showItem" segue
+        switch segue.identifier {
+            case "showItem":
+            // Figure out which row was just tapped
+                if let row = tableView.indexPathForSelectedRow?.row {
+                    // Get the item associated with this row and pass it along
+                    let item = itemStore.allItems[row]
+                    let detailViewController = segue.destination as! DetailViewController
+                    
+                    // dependency injection -- into DetailViewController object
+                    detailViewController.item = item
+                    detailViewController.imageStore = imageStore
+                }
+            default:
+                preconditionFailure("Unexpected segue identifier")
+        }
+    }
+}
